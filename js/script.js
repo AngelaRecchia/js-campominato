8bec565d325d36cd2995eea1fc5602973057e482
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-08 15:42:47 +0200

    add difficoltà

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ Se il numero è presente nella lista dei numeri generati, la partita termina, al
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 BONUS: (da fare solo se funziona tutto il resto) all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50 */
 
-/* dichiarazione variabili */
-
+/* scelta difficoltà */
 var max = 100;
 var min = 1
+do var diff = parseInt(prompt("Scegli il livello di difficoltà: 0, 1 o 2"));
+while (diff != 0 && diff != 1 && diff != 2);
+if (diff == 1) max = 80; 
+else if (diff == 2) max = 50;
+
+/* dichiarazione variabili */
 var numeri = createArray();
 var round = 0;
 var numUtente = 0;
@@ -26,11 +31,11 @@ function createArray(){
 /* richiesta numeri a utente */
 function askNumbers(){
     var array = [];
-    while(round < 100 - 16 && !numeri.includes(numUtente)) {
-        var frase = "Inserisci un numero da 1 a 100";
-        while (numUtente < 1 || numUtente > 100 || Number.isNaN(numUtente) || array.includes(numUtente)){
+    while(round < max - 16 && !numeri.includes(numUtente)) {
+        var frase = "Inserisci un numero da 1 a " + max;
+        while (numUtente < min || numUtente > max || Number.isNaN(numUtente) || array.includes(numUtente)){
             numUtente = parseInt(prompt(frase));
-            frase = "Numero non valido: inserisci un numero da 1 a 100";
+            frase = "Numero non valido: inserisci un numero da 1 a " + max;
         }
         array.push(numUtente);
         round++;
